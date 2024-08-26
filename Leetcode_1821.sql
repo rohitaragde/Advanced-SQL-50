7a65b14ad90164bde4d35cffa8b21563afc5975e
Rohit Annasaheb Ragde <32512875+rohitaragde@users.noreply.github.com>
2024-08-26 19:03:56 -0400

    Update Leetcode_1821.sql

--- Leetcode_1821.sql
+++ Leetcode_1821.sql
@@ -7,6 +7,14 @@ create table Cust
  revenue int,
  primary key(customer_id,yyear));
 
+insert into Cust values(1,2018,50);
+insert into Cust values(1,2021,30);
+insert into Cust values(1,2020,70);
+insert into Cust values(2,2021,-50);
+insert into Cust values(3,2018,10);
+insert into Cust values(3,2016,50);
+insert into Cust values(4,2021,20);
+
 select * from cust;
 
 /* Write a solution to report the customers with positive revenue in the year 2021
