503006e01083bc736ab4097d3698ea959c48435f
Rohit Annasaheb Ragde <32512875+rohitaragde@users.noreply.github.com>
2024-08-30 16:34:24 -0400

    Update leetcode_00182.sql

--- leetcode_00182.sql
+++ leetcode_00182.sql
@@ -9,6 +9,12 @@ insert into Person11 values(1,'a@b.com')
 insert into Person11 values(2,'c@b.com')
 insert into Person11 values(3,'a@b.com')
 
+
+/*Write a solution to report all the duplicate emails.
+ Note that it's guaranteed that the email field is not NULL.*/
+
+Return the result table in any order.
+
 select * from Person11
 
 select email from Person11
