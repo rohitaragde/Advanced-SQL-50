50d087a82cfbeb3197aeb25463183da29e1c3b65
Rohit Annasaheb Ragde <32512875+rohitaragde@users.noreply.github.com>
2024-08-30 16:36:02 -0400

    Update leetcode_01173.sql

--- leetcode_01173.sql
+++ leetcode_01173.sql
@@ -16,7 +16,6 @@ insert into Delivery values(4,3,'2019-08-24','2019-08-26')
 insert into Delivery values(5,4,'2019-08-21','2019-08-22')
 insert into Delivery values(6,2,'2019-08-11','2019-08-13')
 
-select * from Delivery
 
 /*If the customer's preferred delivery date is the same as the order date,
 then the order is called immediate; otherwise, it is called scheduled.
@@ -24,7 +23,21 @@ Write a solution to find the percentage of immediate orders in the table,
 rounded to 2 decimal places.
 */
 
+ select * from Delivery
+
+ SQL Server:-
+
 SELECT
     ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100.00 / COUNT(*), 2) AS immediate_percentage
 FROM 
     Delivery;
+
+MYSQL:-
+
+SELECT
+    ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) / COUNT(*)*100.00, 2) AS immediate_percentage
+FROM 
+    Delivery;
+
+
+
